6c58d8c61460bff97f369f63c66e69041fe53e4b
YeachaN <92733150+chans27@users.noreply.github.com>
2024-04-16 23:21:12 +0900

    Interceptor

---
+++ src/main/java/com/ycblog/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.ycblog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .excludePathPatterns("/foo"); // 이경로의 url은 인증없이 접속 가능
+    }
+}
--- src/main/java/com/ycblog/controller/PostController.java
+++ src/main/java/com/ycblog/controller/PostController.java
@@ -19,6 +19,16 @@ public class PostController {
 
     private final PostService postService;
 
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
+    }
+
+    @GetMapping("/foo")
+    public String foo() {
+        return "foo";
+    }
+
     //auth by Get Parameter
     @PostMapping("/posts")
     public void post(@RequestBody @Valid PostCreate request) {
