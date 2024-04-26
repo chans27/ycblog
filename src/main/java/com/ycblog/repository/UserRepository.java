fc60dbc6eec1d2322917e4883e8dbcb1860799e2
YeachaN <92733150+chans27@users.noreply.github.com>
2024-04-26 22:02:21 +0900

    login request test

--- src/main/java/com/ycblog/repository/UserRepository.java
+++ src/main/java/com/ycblog/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.ycblog.repository;
 import com.ycblog.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,1 @@
-INSERT INTO `member` (created_at, name, email, password)
-VALUES ('2024-04-25 20:00:00', 'chan', 'chan27@naver.com', '1234')
+INSERT INTO `member` (created_at, name, email, password) VALUES ('2024-04-25 20:00:00', 'chan', 'chan27@naver.com', '1234')
